4d426171ad51ad4303ec336710012d92f8758aa9
Jimboga <fernandonahuelsolorzano@gmail.com>
2023-02-27 16:07:11 -0300

    Comente el no nombre de los perros

---
+++ src/main/java/com/mycompany/tareautn/TareaUTN.java
@@ -0,0 +1,22 @@
+package com.mycompany.tareautn;
+
+
+public class TareaUTN {
+
+    public static void main(String[] args) {
+        int numeroInicial = 1;
+        int numeroFinal = 20;
+        
+        //int pares;
+        while (numeroInicial<=numeroFinal){
+            numeroInicial = numeroInicial + 1;
+            System.out.println(numeroInicial);
+            numeroInicial = numeroInicial + 1;
+        }
+        for(int parrevez=20;parrevez>1;parrevez--){
+            //parrevez = parrevez -2;
+            System.out.println(parrevez);
+            parrevez = parrevez -1;
+        }
+    }
+}
